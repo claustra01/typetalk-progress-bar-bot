00c08b19c8fdfebe6d4945e5f5ebbf63fecd16e8
claustra01 <shinya430.g@gmail.com>
2024-07-22 00:22:24 +0900

    create and save image

--- main.go
+++ main.go
@@ -2,11 +2,51 @@ package main
 
 import (
 	"fmt"
+	"image"
+	"image/color"
+	"time"
 
 	"gocv.io/x/gocv"
 )
 
+func createProgressBar() {
+	// 現在の日付を取得
+	now := time.Now()
+	startOfYear := time.Date(now.Year(), time.January, 1, 0, 0, 0, 0, now.Location())
+	endOfYear := time.Date(now.Year(), time.December, 31, 23, 59, 59, 0, now.Location())
+	yearProgress := float64(now.Sub(startOfYear)) / float64(endOfYear.Sub(startOfYear))
+
+	// 画像のサイズを設定
+	width := 1000
+	height := 200
+
+	// 画像を作成
+	img := gocv.NewMatWithSize(height, width, gocv.MatTypeCV8UC3)
+	defer img.Close()
+
+	// 背景を塗りつぶし
+	bgColor := color.RGBA{0, 0, 50, 255}
+	gocv.Rectangle(&img, image.Rect(0, 0, width, height), bgColor, -1)
+
+	// プログレスバーの枠を描画
+	borderColor := color.RGBA{50, 100, 255, 255}
+	gocv.Rectangle(&img, image.Rect(20, 20, width-20, height-20), borderColor, 3)
+
+	// プログレスバーを描画
+	progressBarColor := color.RGBA{50, 100, 255, 255}
+	progressWidth := int(float64(width-40) * yearProgress)
+	gocv.Rectangle(&img, image.Rect(20, 20, 20+progressWidth, height-20), progressBarColor, -1)
+
+	// タイトルを描画
+	title := fmt.Sprintf("%d Progress Bar", now.Year())
+	gocv.PutText(&img, title, image.Point{width/2 - 100, 50}, gocv.FontHersheySimplex, 1.0, borderColor, 2)
+
+	// 画像を保存
+	filename := fmt.Sprintf("progress_bar_%d_%02d_%02d.png", now.Year(), now.Month(), now.Day())
+	gocv.IMWrite(filename, img)
+	fmt.Printf("Progress bar image saved as %s\n", filename)
+}
+
 func main() {
-	fmt.Printf("gocv version: %s\n", gocv.Version())
-	fmt.Printf("opencv lib version: %s\n", gocv.OpenCVVersion())
+	createProgressBar()
 }
